8d3bc0e99561927f6288de9abf716ac4b66d67d4
Swamedia <acepabdurohman90@gmail.com>
2017-11-29 22:57:27 +0700

    tambah unit test

---
+++ src/test/java/com/acepabdurohman/test/TestUrl.java
@@ -0,0 +1,21 @@
+package com.acepabdurohman.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestUrl {
+
+    @Autowired
+    private TestController testController;
+
+    @Test
+    public void testHelloJenkins(){
+        Assert.assertEquals(testController.test(), "hello jenkins");
+    }
+}
